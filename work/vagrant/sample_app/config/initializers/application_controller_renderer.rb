676ab4ca6f1dc936d2f831c925f2917abfdc2dd3
林晃弘 <hayashiakihiro@ampmaco.local>
2019-04-11 14:22:34 +0900

    [Start-Finish] 3-2~9

---
+++ work/vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '96e7f098ea4c192d4cb6f4cc213b160ab15ea1368e5313fb5ffd1672e80a98bfb76cf21bcdafa1457824d6a4946a2591f0df45a64f6bab0e22a54937c2e6ed2e'
